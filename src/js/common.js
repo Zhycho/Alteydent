c6fbf52d3d29a5510c00605ca97aae5a2681cc41
Artem <avd.qu6@gmail.com>
2020-06-18 13:54:36 +0500

    Внёс правки

--- src/js/common.js
+++ src/js/common.js
@@ -237,7 +237,7 @@ $(document).ready(function() {
                         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path><path d="M0 0h24v24H0z" fill="none"></path></svg>
                     </a>
                     <a class="mobile-menu__logo" href="index.html">
-                        <img src="/assets/img/logo-altey_small.png" alt="Логотип стоматологии Алтейдент в виде зуба, объятого лепестками">
+                        <img src="assets/img/logo-altey_small.png" alt="Логотип стоматологии Алтейдент в виде зуба, объятого лепестками">
                     </a>
                     `
                 ]
